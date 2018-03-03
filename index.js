8ffe4a2992f47486abf548f8cd1c6ca0baef3c25
Nick Perez <nickjperez@gmail.com>
2018-03-03 02:24:03 -0500

    feat: template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
+const views = require('koa-views');
 const app = new Koa();
 
+app.use(views(__dirname + '/views'));
+
 app.use(async ctx => {
-  ctx.body = 'Hello Collective';
+  await ctx.render('index');
 });
 
 app.listen(8000);
